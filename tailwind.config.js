6d0cf7cc77cceda07d7ac4f3ecde07836b3cd6d5
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-12-09 00:25:43 +0700

    Debug Tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Manrope: ["Manrope", "sans-serif"],
+        Shippori: ["Shippori Antique B1", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
