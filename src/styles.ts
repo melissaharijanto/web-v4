0d04476992dae29ed65da79f9661c90e04fcb40a
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-12-09 23:08:47 +0700

    Add new text styles

--- src/styles.ts
+++ src/styles.ts
@@ -2,6 +2,10 @@ export const styles = {
     'header-sm': 'text-white font-shippori text-2xl',
     'header-sm-black': 'text-black font-shippori text-2xl',
     'header-xl': 'text-white font-shippori text-8xl',  
+    'subheading': 'text-white font-manrope text-xl font-semibold',
+    'subheading-black': 'text-black font-manrope text-xl font-semibold',
+    'button-text': 'text-white font-manrope text-lg font-semibold',
+    'button-text-black': 'text-black font-manrope text-lg font-semibold',
     'navbar-link': 'text-white font-manrope text-lg hover:font-bold transition-all ease-in relative inline-block group',
     'navbar-underline': "absolute left-0 bottom-0 h-0.5 w-0 bg-white transition-all duration-300 ease-in-out group-hover:w-full",
 }
