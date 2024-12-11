a9fe06bfe4e03b9dced8cfe0bf4edf6a3d76ee22
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-12-12 00:17:21 +0700

    Change page padding top and add see more in resume button

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,13 +1,15 @@
 import ExperienceComponent from '../components/ExperienceComponent';
 import SubheadingTitle from '../components/SubheadingTitle';
+import DiagonalArrow from '../components/svgs/DiagonalArrow';
 import { styles } from '../utils/css/styles';
 import { experiences } from '../utils/data/experience';
+import { links } from '../utils/data/links';
 
 const Experience = () => {
     return (
         <div
             id="experience"
-            className={`${styles.page} flex flex-col justify-center items-center`}>
+            className={`${styles.page} flex flex-col justify-center items-center pt-28`}>
             <SubheadingTitle
                 title="EXPERIENCE"
                 flowerFill="fill-blue"></SubheadingTitle>
@@ -27,6 +29,13 @@ const Experience = () => {
                     );
                 })}
             </div>
+            <a
+                href={links.resume}
+                target="_blank"
+                className={`${styles['button-text']} ${styles['button']} ${styles['button-with-diagonal-arrow']} mt-10`}>
+                <span className="mb-1">See more in my resume</span>
+                <DiagonalArrow width="w-3" fill="fill-white"></DiagonalArrow>
+            </a>
         </div>
     );
 };
