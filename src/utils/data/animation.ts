70d9669c63b6bfc4d23eee749246d1ea9d886bec
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-12-13 01:41:25 +0700

    Add more animation variants and modify old ones' values

--- src/utils/data/animation.ts
+++ src/utils/data/animation.ts
@@ -1,5 +1,5 @@
 export const childVariants = {
-    hidden: { x: '10vw', opacity: 0 },
+    hidden: { x: '5vw', opacity: 0 },
     visible: {
         x: 0,
         opacity: 1,
@@ -7,7 +7,49 @@ export const childVariants = {
     },
     exit: {
         opacity: 0,
-        x: '-10vw',
+        x: '-5vw',
+        transition: { type: 'tween', duration: 1 },
+    },
+};
+
+export const childVariantsTwo = {
+    hidden: { x: '-5vw', opacity: 0 },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: { type: 'tween', duration: 1 },
+    },
+    exit: {
+        opacity: 0,
+        x: '5vw',
+        transition: { type: 'tween', duration: 1 },
+    },
+};
+
+export const childVariantsThree = {
+    hidden: { y: '5vh', opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: { type: 'tween', duration: 1 },
+    },
+    exit: {
+        opacity: 0,
+        y: '-5vh',
+        transition: { type: 'tween', duration: 1 },
+    },
+};
+
+export const childVariantsFour = {
+    hidden: { y: '-5vh', opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: { type: 'tween', duration: 1 },
+    },
+    exit: {
+        opacity: 0,
+        y: '5vh',
         transition: { type: 'tween', duration: 1 },
     },
 };
