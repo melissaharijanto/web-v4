32cf7559424ecc0e6d113d76e790a16a5532072a
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-12-14 07:33:29 +0700

    Add spinning text ring component

---
+++ src/components/SpinningTextRing.tsx
@@ -0,0 +1,21 @@
+import Sparkle from './svgs/Sparkle';
+import TextRing from './svgs/TextRing';
+
+const SpinningTextRing = () => {
+    return (
+        <div className="relative flex items-center justify-center w-24 rounded-full">
+            <div className="w-full flex justify-center items-center">
+                <Sparkle width="w-16" fill="fill-yellow" />
+            </div>
+            <div className="absolute w-full h-full flex items-center justify-center">
+                <TextRing
+                    width="w-auto"
+                    className="flex items-center justify-center animate-spin-slower"
+                    fill="fill-white"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default SpinningTextRing;
