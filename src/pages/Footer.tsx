371f22584daa725516ad554110c7ca20862fe18f
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-12-31 00:33:31 +0700

    Add animation

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
                             </p>
                             <a
                                 href={links.email}
-                                className="border-2 border-white rounded-full p-3 lg:p-4 group hover:border-yellow transition-all ease-in mt-3">
+                                className="border-2 border-white rounded-full p-3 lg:p-4 group hover:border-yellow transition-all ease-in mt-3 hover:motion-safe:animate-pulse">
                                 <DiagonalArrow
                                     width="w-6 lg:w-10"
                                     fill={
