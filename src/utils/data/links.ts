ad1a72b071ec4121e618629f453467118760568a
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-12-30 20:35:15 +0700

    Add footer links

--- src/utils/data/links.ts
+++ src/utils/data/links.ts
@@ -5,3 +5,72 @@ export const links = {
     'email': 'mailto:melissaharijanto18@gmail.com',
     'sq-recommendation': 'https://drive.google.com/file/d/11hhdBfcenwhzsAIwDMqDT_SwPuNckrVF/view?usp=sharing'
 }
+
+export const footerLinks = {
+    one: [
+        {
+            label: "Software Engineering",
+            link: ""
+        },
+        {
+            label: "Front-end Development",
+            link: ""
+        },
+        {
+            label: "Full-stack Development",
+            link: ""
+        },
+        {
+            label: "UI/UX Design",
+            link: ""
+        }
+    ],
+    two: [
+        {
+            label: "Email",
+            link: "mailto:melissaharijanto18@gmail.com"
+        },
+        {
+            label: "GitHub",
+            link: "https://github.com/melissaharijanto"
+        },
+        {
+            label: "LinkedIn",
+            link: "https://linkedin.com/in/melissaharijanto"
+        },
+        {
+            label: "Resume",
+            link: 'https://drive.google.com/file/d/11TjWo-55mOeTio7dKnydA2qOFplJOjAx/view?usp=drive_link',
+        },
+        {
+            label: "Behance",
+            link: "https://www.behance.net/melisara"
+        },
+    ],
+    three: [
+        {
+            label: "Old Portfolio Website",
+            link: ""
+        }, 
+        {
+            label: "Travel Log",
+            link: "https://github.com/melissaharijanto/Travel-Log/releases/tag/v1.0"
+        }, 
+        {
+            label: "PINUS Study",
+            link: "https://pinusstudy.com"
+        },
+        {
+            label: "Feminnovate",
+            link:"https://feminnovate-frontend.vercel.app/"
+        },
+        {
+            label: "Classroom 360",
+            link:"https://classroom-360.netlify.app"
+        },
+        {
+            label: "TrackO Desktop App",
+            link:"https://github.com/AY2223S1-CS2103T-W15-3/tp/releases/tag/v1.4"
+        } 
+    ],
+}
